12f94619385fb1b442331b73771e11b9c9835dc2
razzy777 <johannes@wecreate.media>
2024-11-01 18:30:19 +0100

    n

--- relay_test.js
+++ relay_test.js
@@ -1,13 +1,13 @@
-const Gpio = require('onoff').Gpio;
+const Gpio = require('onoff').Gpio
 
-const relay = new Gpio(17, 'out'); // GPIO 17 connected to IN1
-
-// Activate the relay (turn ON solenoid valve)
-relay.writeSync(1);
-
-// Wait for 5 seconds
-setTimeout(() => {
-  // Deactivate the relay (turn OFF solenoid valve)
-  relay.writeSync(0);
-  relay.unexport();
-}, 5000);
+try {
+	const relay = new Gpio(17, 'out')
+	relay.writeSync(1) // Activate relay
+	setTimeout(() => {
+		relay.writeSync(0) // Deactivate relay after 3 seconds
+		relay.unexport()
+		console.log('Relay test completed')
+	}, 3000)
+} catch (error) {
+	console.error('Error with GPIO:', error)
+}
