bd207d019433edf3e5dad3e876b0b50b7333f081
razzy777 <johannes@wecreate.media>
2024-11-03 17:37:03 +0100

    d

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,17 +5,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-  safelist: [
-    'bg-blue-500',
-    'bg-blue-600',
-    'bg-blue-700',
-    'bg-gray-500',
-    'bg-gray-600',
-    'bg-gray-700',
-    'hover:bg-blue-600',
-    'hover:bg-blue-700',
-    'hover:bg-gray-600',
-    'focus:ring-blue-400',
-    'focus:ring-gray-400',
-  ]
 }
