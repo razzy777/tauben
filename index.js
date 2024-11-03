bced7151e062e098c352c8048f524a7f1e6b9b3d
razzy777 <johannes@wecreate.media>
2024-11-03 15:35:24 +0100

    ff

--- index.js
+++ index.js
@@ -63,12 +63,19 @@ io.on('connection', (socket) => {
   });
 
   socket.on('moveServoRelative', async ({ pan, tilt }) => {
+    if (servoLock) {
+        console.log('Servo is currently busy. Please wait for the current movement to complete.');
+        return;
+    }
+    servoLock = true; // Acquire the lock    
     try {
       console.log(`Moving servo to RELATIVE position: pan=${pan}, tilt=${tilt}`);
       await moveToPositionRelative(pan, tilt);
       console.log('Servo moved to requested position.');
     } catch (error) {
       console.error('Error moving servo:', error);
+    } finally {
+        servoLock = false; // Release the lock after the operation completes
     }
   });
 
