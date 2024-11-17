6d52b69361f4b0a6ce427a8d32427543ec70d0c1
razzy777 <johannes@wecreate.media>
2024-11-17 10:23:20 +0100

    dd

--- ai_processor.py
+++ ai_processor.py
@@ -279,7 +279,7 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument(
         "-n", "--net",
         help="Path for the network in HEF format.",
-        default="/Downloads/yolov8n.hef"
+        default="/home/johannes/Downloads/yolov8n.hef"
     )
     parser.add_argument(
         "-l", "--labels",
